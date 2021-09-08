c6e4e47c27c0272925ea6db232075842499b65dd
helderjosue <helderjosuemata@gmail.com>
2021-09-08 11:32:42 +0000

    last update changing login page

--- backend/views/layouts/auth.php
+++ backend/views/layouts/auth.php
@@ -1,3 +1,4 @@
+
 <?php
 
 /* @var $this \yii\web\View */
@@ -9,24 +10,26 @@ use yii\bootstrap4\Nav;
 use yii\bootstrap4\NavBar;
 use yii\widgets\Breadcrumbs;
 use common\widgets\Alert;
-
+ 
 AppAsset::register($this);
 
 $this->beginContent('@backend/views/layouts/base.php');
 ?>
 
 
-
+<div class="wrap h-100 d-flex flex-column">
+    <!-- NAVBAR RETIRADA DAQUI -->
+  <!-- header estava aqui -->
 
 <!-- CRIACAO DO SIDEBAR E APLICACAO DE ESTILOS -->
     <main class = "d-flex">
-    <?php echo $this->render('sidebar')?>
 
-    <div class="content-wrapper p-3">
+    <!-- <div class="content-wrapper p-3">  content-wrapper faz o form ter full width-->
+    <div class=" p-3">
       
       <?= Alert::widget() ?>
       <?= $content ?>
-
+  </div>
 
     </main>
    
---
+++ backend/views/layouts/sidebar.php
@@ -0,0 +1,57 @@
+<style>
+
+aside .nav-pills .nav-link {
+    border-radius: 0;
+    color: #444444;
+}
+
+aside .nav-pills .nav-link:hover {
+    background: rgba(0, 0, 0, 0.05);
+}
+
+aside .nav-pills .nav-link.active{
+
+    background: rgba(0, 0, 0, 0.05);
+    color: #b90000;
+    border-left: 4px solid #b90000;
+}
+
+aside{
+  min-width: 200px;
+}
+.content-wrapper{
+    flex: 1;
+}
+main{
+  flex:1;
+}
+</style>
+<aside class = "shadow">
+
+<?php
+
+echo \yii\bootstrap4\Nav::widget([
+
+    'options' => [
+        'class' => 'd-flex flex-column nav-pills'
+    ],
+    'items' => [
+        [
+        'label' => 'Dashboard', 'url' => ['/site/index']
+    ],
+    [
+        'label' => 'Videos', 'url' => ['/video/index']
+    ]
+    ]
+]) ?>
+</aside>
+<!-- 
+<div class="list-group">
+  <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+    The current link item
+  </a>
+  <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+  <a href="#" class="list-group-item list-group-item-action">A third link item</a>
+  <a href="#" class="list-group-item list-group-item-action">A fourth link item</a>
+  <a href="#" class="list-group-item list-group-item-action disabled" tabindex="-1" aria-disabled="true">A disabled link item</a>
+</div> -->
